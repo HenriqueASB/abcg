41783f69c8ba1c7ae884104e2ef649218526e88b
Henrique <henriquebatista2504@hotmail.com>
2022-12-03 17:02:52 -0300

    atividade 4 malha gerada em codigo

---
+++ examples/atividade4/model.cpp
@@ -0,0 +1,140 @@
+#include "model.hpp"
+
+#include <unordered_map>
+
+// Explicit specialization of std::hash for Vertex
+template <> struct std::hash<Vertex> {
+  size_t operator()(Vertex const &vertex) const noexcept {
+    auto const h1{std::hash<glm::vec3>()(vertex.position)};
+    return h1;
+  }
+};
+
+void Model::createBuffers() {
+  // Delete previous buffers
+  abcg::glDeleteBuffers(1, &m_EBO);
+  abcg::glDeleteBuffers(1, &m_VBO);
+
+  // VBO
+  abcg::glGenBuffers(1, &m_VBO);
+  abcg::glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
+  abcg::glBufferData(GL_ARRAY_BUFFER,
+                     sizeof(m_vertices.at(0)) * m_vertices.size(),
+                     m_vertices.data(), GL_STATIC_DRAW);
+  abcg::glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+  // EBO
+  abcg::glGenBuffers(1, &m_EBO);
+  abcg::glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_EBO);
+  abcg::glBufferData(GL_ELEMENT_ARRAY_BUFFER,
+                     sizeof(m_indices.at(0)) * m_indices.size(),
+                     m_indices.data(), GL_STATIC_DRAW);
+  abcg::glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+}
+
+void Model::loadObj(bool standardize) {
+  m_vertices.clear();
+  m_indices.clear();
+
+  int n = 10;
+
+  for(int i = 0; i <= n; i ++){
+      for(int j = 0; j <= n; j ++){
+        m_vertices.push_back(Vertex{.position = {(j/(float)n) ,(i/(float)n),0.0f}});
+  }
+  }
+
+  for(int k =0; k < ((n)*(n+1)); k ++){
+      if(((k+1)%(n+1))){
+       m_indices.push_back(k);
+       m_indices.push_back(k+n+2);
+       m_indices.push_back(k+n+1);
+       m_indices.push_back(k);
+       m_indices.push_back(k+1);
+       m_indices.push_back(k+n+2);
+      }  
+  }
+ 
+
+
+  auto const seed{std::chrono::steady_clock::now().time_since_epoch().count()};
+  m_randomEngine.seed(seed);
+  
+  randomZ();
+
+  if (standardize) {
+    Model::standardize();
+  }
+
+  createBuffers();
+}
+
+void Model::randomZ(){
+  std::uniform_real_distribution rd(0.f, 0.5f);
+
+  for (auto &vertex : m_vertices) {
+    vertex.position = {vertex.position.x, vertex.position.y, rd(m_randomEngine)};
+  }
+}
+
+void Model::render(int numTriangles) const {
+  abcg::glBindVertexArray(m_VAO);
+
+  auto const numIndices{(numTriangles < 0) ? m_indices.size()
+                                           : numTriangles * 3};
+
+  abcg::glDrawElements(GL_TRIANGLES, numIndices, GL_UNSIGNED_INT, nullptr);
+
+  abcg::glBindVertexArray(0);
+}
+
+void Model::setupVAO(GLuint program) {
+  // Release previous VAO
+  abcg::glDeleteVertexArrays(1, &m_VAO);
+
+  // Create VAO
+  abcg::glGenVertexArrays(1, &m_VAO);
+  abcg::glBindVertexArray(m_VAO);
+
+  // Bind EBO and VBO
+  abcg::glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_EBO);
+  abcg::glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
+
+  // Bind vertex attributes
+  auto const positionAttribute{
+      abcg::glGetAttribLocation(program, "inPosition")};
+  if (positionAttribute >= 0) {
+    abcg::glEnableVertexAttribArray(positionAttribute);
+    abcg::glVertexAttribPointer(positionAttribute, 3, GL_FLOAT, GL_FALSE,
+                                sizeof(Vertex), nullptr);
+  }
+
+  // End of binding
+  abcg::glBindBuffer(GL_ARRAY_BUFFER, 0);
+  abcg::glBindVertexArray(0);
+}
+
+void Model::standardize() {
+  // Center to origin and normalize largest bound to [-1, 1]
+
+  // Get bounds
+  glm::vec3 max(std::numeric_limits<float>::lowest());
+  glm::vec3 min(std::numeric_limits<float>::max());
+  for (auto const &vertex : m_vertices) {
+    max = glm::max(max, vertex.position);
+    min = glm::min(min, vertex.position);
+  }
+
+  // Center and scale
+  auto const center{(min + max) / 2.0f};
+  auto const scaling{2.0f / glm::length(max - min)};
+  for (auto &vertex : m_vertices) {
+    vertex.position = (vertex.position - center) * scaling;
+  }
+}
+
+void Model::destroy() const {
+  abcg::glDeleteBuffers(1, &m_EBO);
+  abcg::glDeleteBuffers(1, &m_VBO);
+  abcg::glDeleteVertexArrays(1, &m_VAO);
+}
